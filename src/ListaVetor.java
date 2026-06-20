5487a38c8d131773d5a77c42a6832a2fcec72613
Juninho <junior.bernardo133@hotmail.com>
2026-06-20 12:31:33 -0300

    adiciona intrerface lista

--- src/ListaVetor.java
+++ src/ListaVetor.java
@@ -1,9 +1,8 @@
-public class ListaVetor<T> {
-
-    @SuppressWarnings("unchecked")
+public class ListaVetor<T> implements Lista<T> {
     private T[] itens = (T[]) new Object[100];
     private int tamanho = 0;
 
+    @Override
     public void add(T valor, int pos) {
         for (int i = tamanho; i > pos; i--) {
             itens[i] = itens[i - 1];
@@ -12,15 +11,18 @@ public class ListaVetor<T> {
         tamanho++;
     }
 
+    @Override
     public void add(T valor) {
         itens[tamanho] = valor;
         tamanho++;
     }
 
+    @Override
     public T get(int pos) {
         return itens[pos];
     }
 
+    @Override
     public T remove(int pos) {
         T valor = itens[pos];
         for (int i = pos; i < tamanho - 1; i++) {
@@ -30,6 +32,7 @@ public class ListaVetor<T> {
         return valor;
     }
 
+    @Override
     public boolean remove(T valor) {
         for (int i = 0; i < tamanho; i++) {
             if (itens[i].equals(valor)) {
@@ -40,6 +43,7 @@ public class ListaVetor<T> {
         return false;
     }
 
+    @Override
     public int size() {
         return tamanho;
     }
